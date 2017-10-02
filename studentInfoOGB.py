59c4c7353a0e4e9475594728b97f237ef1e6e9f5
Ogb26 <32171182+Ogb26@users.noreply.github.com>
2017-10-02 10:37:29 -0400

    Student Sorter
    This program lists randomized students by their first name, last name, and age.

--- studentInfoOGB.py
+++ studentInfoOGB.py
@@ -1,5 +1,16 @@
 import random
 
+firstName = ["jay", "jim", "roy", "axel", "billy", "charlie", "jax", "gina", "paul",
+"ringo", "ally", "nicky", "cam", "ari", "trudie", "cal", "carl", "lady", "lauren",
+"ichabod", "arthur", "ashley", "drake", "kim", "julio", "lorraine", "floyd", "janet",
+"lydia", "charles", "pedro", "bradley"]
+
+lastName = ["blake", "holt", "meilijson", "brannon", "fieser", "line", "hellman",
+"lyndon", "wendell", "corr", "fricano", "carrigan", "giddings", "piesse", "charlwood",
+"thaler", "ambrose", "hamer", "schweig", "geiger", "kinder", "rogantin", "chevallier"
+"wang", "tiedemann", "cohn", "digel", "reheem", "dzieciololowski", "callanan",
+"lie", "oden"]
+
 def main():
   students = [
     Student("Larsson", 37),
@@ -7,22 +18,22 @@ def main():
   ]
 
   printHeader()
-  selection = getUserSelection()
+  selection = int(getUserSelection())
   if selection == 0:
     printStudentsByAge(students)
   elif selection == 1:
-    pass
+    printStudentsByLName(students)
   elif selection == 2:
-    pass
+    printStudentsByFName(students)
   else:
-    print "SELECTION NOT RECOGNIZED"
+    print ("SELECTION NOT RECOGNIZED")
 
 
 class Student:
   def __init__(self, lastName, age):
-    self.lastName = lastName
-    self.age = age
-    self.firstName = "JOHN"
+    self.lastName = random.choice (lastName)
+    self.age = self.assignRandomAge()
+    self.firstName = random.choice(firstName)
 
   def assignRandomName(self):
     pass
@@ -30,13 +41,13 @@ class Student:
   def assignRandomAge(self):
     self.age = random.randint(0,100)
 
-  def assignRandomWeight(self, isMetric):
-    pass
+  def assignRandomWeight(self):
+    self.weight = random.randint(0,300)
 
-  def assignRandomHeight(self, isMetric):
-    pass
+  def assignRandomHeight(self):
+    self.height = random.randint(0,84)
 
-inputQuestions = [ 
+inputQuestions = [
   "For STUDENTS BY AGE, type 0",
   "For STUDENTS BY LAST NAME, type 1",
   "For STUDENTS BY FIRST NAME, type 3",
@@ -57,13 +68,20 @@ def printStudentsByAge(students):
   print ("----Students By Age-----")
   sortStudents = sorted(students, key=lambda student: student.age)
   for student in students:
-    print student.lastName + ", " + student.firstName + ", " + str(student.age)
+    print (student.lastName + ", " + student.firstName + ", " + str(student.age))
 
 def printStudentsByLName(students):
-  print ("----Students By -----")
+  print ("----Students By Last Name -----")
+  sortStudentsLName = sorted(students, key=lambda student: student.lastName)
+  for student in students:
+    print  (student.lastName + ", " + student.firstName + ", " + str(student.age))
 
 def printStudentsByFName(students):
-  print ("----Students By -----")
+  print ("----Students By First Name ----")
+  sortStudentsFName = sorted(students, key=lambda student: student.firstName)
+  for student in students:
+    print (student.lastName + ", " + student.firstName + ", " + str(student.age))
+
 
 def printSumAge(students):
   print ("Answer:")
